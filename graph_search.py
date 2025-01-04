5892db9a09293d299fa8085a2e9e6a70cc85d478
viotsoft <viotsoft@gmail.com>
2025-01-04 18:31:12 +0200

    go

---
+++ graph_search.py
@@ -0,0 +1,34 @@
+from collections import deque
+
+
+graph = {
+    'you': ['alice', 'bob', 'claire'],
+    'bob': ['anuj', 'peggy'],
+    'alice': ['peggy'],
+    'claire': ['thom', 'jonny'],
+    'anuj': [],
+    'peggy': [],
+    'thom': [],
+    'jonny': []
+}
+
+def search(name):
+    search_queue = deque()
+    search_queue += graph[name]
+    searched = []
+    while search_queue:
+        person = search_queue.popleft()
+        if not person in searched:
+            if person_is_seller(person):
+                print(person + ' is a mango seller!')
+                return True
+            else:
+                search_queue += graph[person]
+                searched.append(person)
+    return False
+
+def person_is_seller(name):
+    return name[-1] == 'm'  # Example condition to identify a seller
+print(search('you'))
+print(search('anuj'))
+    
